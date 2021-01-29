5516d8a2001bfaf0fcbe341161dedf78f0116fb6
olivia <78116931+obenson891@users.noreply.github.com>
2021-01-29 16:33:20 -0600

    Rotator
    Rotator added

---
+++ Assets/Scripts/Pacer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;using System.Collections;public class Pacer : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private float zMax = 7.5f;
+    private float zMin = -7.5f;
+    private int direction = 1;
+
+
+    void Update()
+    {
+        float zNew = transform.position.z + direction * speed * Time.deltaTime;
+        if (zNew >= zMax)
+        {
+            zNew = zMax;
+            direction *= -1;
+        }
+        else if (zNew <= zMin)
+        {
+            zNew = zMin;            direction *= -1;
+
+        }
+        transform.position = new Vector3(7.5f, 0.75f, zNew);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!!!";
+        }
+    }
+
+
+
+}
